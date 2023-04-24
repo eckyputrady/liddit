16caab211b49c0e1761418393a132f2c294556ce
ecky putrady <ecky@eckys-MacBook-Pro.local>
2023-04-24 10:50:29 +0800

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
-import "./App.css";
-import "boxicons";
-import {
-  TransactionForm,
-  Transaction,
-  TransactionListItem,
-  TransactionCrud,
-} from "./components/transaction/Transaction";
-import {
-  Autocomplete,
-  useAutocomplete,
-} from "./components/autocomplete/Autocomplete";
-import { Header } from "./components/header/Header";
-import { Modal } from "./components/modal/Modal";
-import { ContactUs } from "./components/contactus/ContactUs";
-import { ContactUsWidget } from "./components/contactus-widget/ContactUsWidget";
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 function App() {
-  const autoCompleteProps = useAutocomplete([
-    "washington",
-    "johannesburg",
-    "tokyo",
-    "taipei",
-  ]);
-  return (
-    <div className="app">
-      {/* <TransactionCrud /> */}
-      <Header />
-      <Autocomplete {...autoCompleteProps} />
-      <ContactUsWidget />
-    </div>
-  );
+  return <div className="app">Hello world</div>;
 }
 
 export default App;
