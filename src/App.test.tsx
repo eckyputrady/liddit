caa48fbe3a9782719994bbb401b6c9e99e87ffb9
ecky putrady <ecky@eckys-MacBook-Pro.local>
2023-05-07 23:34:24 +0800

    Remove src so that people can review

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, test, expect, vi } from "vitest";
-
-describe("autocomplete", () => {
-  test("test", () => {
-    expect(1).toBe(1);
-  });
-});
